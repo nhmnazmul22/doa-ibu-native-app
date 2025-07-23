6d2b635e83bf7518674f2dde43b8f6cb65cd62fa
Nhm Nazmul <125717859+nhmnazmul22@users.noreply.github.com>
2025-07-23 17:17:10 +0600

    Delete Backend/app/config/config.js

--- Backend/app/config/config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = 3001;
-export const MAX_JSON_SIZE = "100mb";
-export const URL_ENCODED = true;
-export const REQUEST_LIMIT_TIME = 15 * 60 * 1000; // 15 Min (MS)
-export const REQUEST_LIMIT_NUMBER = 30000; // Per 15 Min 3000 Request Allowed
-export const WEB_CACHE = false;
-export const MONGODB_CONNECTION =
-  "mongodb+srv://legality:MM5vjBXhsB6F2001@cluster0.bour0ot.mongodb.net/doaibu";
